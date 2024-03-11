8e2eb9950ec726155c379c527e2253512f8f9f8b
Wuxw <43459224+flamewei123@users.noreply.github.com>
2024-03-11 22:36:55 +0800

    Create 1_run_attribution.sh

---
+++ src/1_run_attribution.sh
@@ -0,0 +1,9 @@
+python 1_calculate_attribution.py \
+    --model_name_or_path ../data/model/ep10_bert_large \
+    --priv_data_path ../data/sampled_TEL.json \
+    --output_dir ../Enorn_tel_bert_large_ep10_results/ \
+    --output_prefix Enorn_tel_bert_large_ep10 \
+    --gpus 4 \
+    --max_seq_length 128 \
+    --batch_size 20 \
+    --num_batch 1 \
